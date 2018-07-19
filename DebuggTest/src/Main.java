a56a0e4e944f135b7ef2ccdb38d569f2fd59d79b
aakulasaikiran <aakulasaikiran>
2018-07-19 18:17:40 +0530

    commit6

---
+++ DebuggTest/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int a = 5;
+		int b = 0;
+
+		for (int i = 0; i <= 5; i++) {
+			b += i * 2;
+			System.out.println(a + " " + b + " " + i);
+		}
+		if (a > 0) {
+			System.out.println("in side iff");
+		}
+
+	}
+
+}
